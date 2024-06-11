f52f9b56e40c7acd742b5cabccaa30f2ce1c865e
israelli <li18566230175@gmail.com>
2024-06-11 16:21:08 +0800

    bugfix(table): fixed-bottom & 函数调用为空判定

--- packages/table/src/hooks/use-column-template.tsx
+++ packages/table/src/hooks/use-column-template.tsx
@@ -23,7 +23,7 @@
  * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
-import { ComponentInternalInstance, VNode, VNodeNormalizedChildren, getCurrentInstance, isVNode, unref } from 'vue';
+import { VNode, isVNode, unref } from 'vue';
 
 import { v4 as uuidv4 } from 'uuid';
 
