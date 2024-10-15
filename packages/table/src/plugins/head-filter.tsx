7437341e03a79fb986d65b910ebb1ae74ab4157b
israelli <li18566230175@gmail.com>
2024-10-15 16:05:51 +0800

    fix: 合并冲突

--- packages/table/src/plugins/head-filter.tsx
+++ packages/table/src/plugins/head-filter.tsx
@@ -135,6 +135,12 @@ export default defineComponent({
       return defaultMin;
     });
 
+    const contentStyle = computed(() => ({
+      maxHeight: `${maxHeight.value}px`,
+      minHeight: `${minHeight.value}px`,
+      height: /%$/.test(`${height.value}`) ? height.value : `${height.value}px`,
+    }));
+
     const getRegExp = (val: boolean | number | string, flags = 'ig') =>
       new RegExp(`${val}`.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&'), flags);
 
@@ -282,8 +288,8 @@ export default defineComponent({
     };
 
     const renderFilterList = scope => {
-      if (scope.data.length) {
-        return scope.data.map((item: { value: string; text: string; tipKey?: string }) => (
+      if (scope.length) {
+        return scope.map((item: { value: string; text: string }) => (
           <div
             key={item.value}
             ref={filterPopoverRef}
@@ -330,21 +336,12 @@ export default defineComponent({
                 <Input v-model={searchValue.value}></Input>
               </div>
               <BkCheckboxGroup class='content-list'>
-                <VirtualRender
-                  ref={refVirtualRender}
-                  height={height.value}
-                  className='content-items'
-                  lineHeight={32}
-                  list={localData.value}
-                  maxHeight={maxHeight.value}
-                  minHeight={minHeight.value}
-                  scrollEvent={true}
-                  throttleDelay={0}
+                <div
+                  class='content-items'
+                  style={contentStyle.value}
                 >
-                  {{
-                    default: renderFilterList,
-                  }}
-                </VirtualRender>
+                  {renderFilterList(localData.value)}
+                </div>
               </BkCheckboxGroup>
               <div class='content-footer'>
                 {renderSaveBtn()}
